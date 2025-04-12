943578ccf2bea43e4a0cc7bfe3a259d2138e7985
Aayush Pandey <96182544+aayushpandey01@users.noreply.github.com>
2025-04-12 14:17:43 +0530

    Revert "Add files via upload"

--- wrangler-core/src/main/antlr4/io/cdap/wrangler/parser/SWE assignment Zeotap/RecipeParserVisitor.java
+++
@@ -1,9 +0,0 @@
-@Override
-public TokenGroup visitByteSizeArg(DirectivesParser.ByteSizeArgContext ctx) {
-    return new TokenGroup().add(new ByteSize(ctx.getText()));
-}
-
-@Override
-public TokenGroup visitTimeDurationArg(DirectivesParser.TimeDurationArgContext ctx) {
-    return new TokenGroup().add(new TimeDuration(ctx.getText()));
-}
